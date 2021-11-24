497d04a854949ed38e5b7dd668be2a7742872de9
DiegoLSnetwork <diego.gutierrez@smartnetworksas.com>
2021-11-23 22:55:35 -0500

    ejs for product and dashboard ready for front development

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const express=require("express");
-const app= express();
-const port = 5000;
+const express = require("express");
+const app = express();
 
 // Settings
-app.set("appName","Tienda")
-app.set("port",4242)
-app.set("view engine","ejs")
+app.set("appName", "Tienda")
+app.set("port", 4242)
+app.set("view engine", "ejs")
 
 // Middlewares
 app.use(express.json());
 
-function logger(req, res, next){
+function logger(req, res, next) {
     console.log("request recieved");
     next();
 }
-function productSubmit(req,res){
+function productSubmit(req, res) {
 
 }
 
 // Routes
-app.get("/",(req,res)=>{
-    data=[{name:"Dan"},{name:"Hal"},{name:"Roy"}]
-    res.render("index.ejs",{users:data});
+app.get("/", (req, res) => {
+    data = [{ name: "Dan" }, { name: "Hal" }, { name: "Roy" }]
+    res.render("index.ejs", { users: data });
 });
 
 app.use(logger);
 
-app.get("/dashboard",(req,res)=>{
-    data=[{id:1,productName:"Pollo",quantity:30, store:"Bogotá"},{id:2,productName:"Papa",quantity:1200, store:"Cali"},{id:3,productName:"Coco",quantity:25, store:"Medellin"}]
-    res.render("dashboard.ejs",{products:data});
+app.get("/dashboard", (req, res) => {
+    data=[{id:1,cityName:"Bogotá",stats:{profits:20000000,clients:1654,stock:589}},{id:2,cityName:"Cali",stats:{profits:35000000,clients:1343,stock:349}}]
+    res.render("dashboard.ejs",{cities:data});
 });
 
-app.get("/products", (req,res)=>{
-    res.send("<h1>Products</h1>");
+app.get("/products", (req, res) => {
+    data = [{ id: 1, productName: "Pollo", quantity: 30, store: "Bogotá" }, { id: 2, productName: "Papa", quantity: 1200, store: "Cali" }, { id: 3, productName: "Coco", quantity: 25, store: "Medellin" }]
+    res.render("products.ejs", { products: data });
 });
-app.post("/products",(req,res)=>{
+app.post("/products", (req, res) => {
 
 })
 
-app.listen(app.get("port"),()=>{
+app.listen(app.get("port"), () => {
     console.log(`App listening at port ${app.get("port")}`)
 });
